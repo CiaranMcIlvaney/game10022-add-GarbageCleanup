d5a5d00d58602845d1543ac4fc673ef25385ebe9
Isaac <isaac.ebert@mohawkcollege.ca>
2026-03-09 15:42:57 -0400

    Squashed commit of the following:
    commit 8a2aeeb4adc7d16ef2471a78cb8ebd7146ac3072
    Author: Isaac <isaac.ebert@mohawkcollege.ca>
    Date:   Mon Mar 9 15:40:41 2026 -0400
    
        Add open and close animation to bins
    
    commit 00a1a6e1bf4ed368866003f77eb84159ab996bf4
    Merge: 26ea83c c139f2b
    Author: Isaac <isaac.ebert@mohawkcollege.ca>
    Date:   Mon Mar 9 14:51:27 2026 -0400
    
        Merge branch 'main' into Isaac
    
    commit 26ea83c68a75051592d9638dc8eaf73003ef8820
    Author: Isaac <isaac.ebert@mohawkcollege.ca>
    Date:   Mon Mar 9 14:46:10 2026 -0400
    
        Fix scene changing and add guide + animation
    
    commit 03f9858ba68074737b1358e68f0aa8b2b57a91d9
    Author: Isaac <isaac.ebert@mohawkcollege.ca>
    Date:   Fri Mar 6 15:50:36 2026 -0500
    
        Add base guide toggling (no functionality)

--- GarbageCleanup/Assets/Scripts/BinController.cs
+++ GarbageCleanup/Assets/Scripts/BinController.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class BinController : MonoBehaviour
 {
     [Header("Waste Type")]
-    
+
     // What type of garbage this bin accepts
     public Garbage acceptedType = Garbage.Waste;
 
+    [Header("Animation")]
+    public Animator lidAnimator;
+
     // Called when the player interacts with the bin
     public void TryDeposit(InventoryController inventory)
-    { 
+    {
         // Ask inventory what type is currently selected
-        Garbage ? currentType = inventory.GetCurrentGarbageType();
+        Garbage? currentType = inventory.GetCurrentGarbageType();
 
         // If null then the player has nothing selected / the inventory is empty
         if (currentType == null)
@@ -39,12 +42,29 @@ public class BinController : MonoBehaviour
             Debug.Log($"SUCCESS! Deposited {currentType.Value} into {acceptedType} bin.");
             ScoreManager.Instance.AddCorrect(currentType.Value);
         }
-        
+
         // Wrong bin
         else
         {
             Debug.Log($"FAIL! You tried to deposit {currentType.Value} into {acceptedType} bin.");
             ScoreManager.Instance.AddWrong(currentType.Value);
+        }
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.name == "Player")
+        {
+            // Open the lid
+            lidAnimator.Play("LidOpen");
+        }
+    }
+    private void OnTriggerExit(Collider player)
+    {
+        if (player.gameObject.name == "Player")
+        {
+            // Close the lid
+            lidAnimator.Play("LidClose");
         }
     }
 }
